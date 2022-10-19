8bd50d166d3eae8f2fab5c9527db56d345cdee29
Godwill <dollarcho@gmail.com>
2022-10-18 21:11:47 -0500

    bug fixes

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		String name = "vamsi"
 		system.out.println(name);
+		Integer age = "20";
+		system.out.println(age + 5);
 	}
 
 }
